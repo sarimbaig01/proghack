2e52d57a130bc292f1992e87d330d2490598e953
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2024-07-25 15:53:45 +0100

    Create gol3.cpp

---
+++ projs/cpp/gol3.cpp
@@ -0,0 +1,4 @@
+int calculatePopulation(const std::vector<std::vector<int>>& grid);
+double calculateGrowthRate(const std::vector<std::vector<int>>& currentGrid, const std::vector<std::vector<int>>& previousGrid);
+double calculateStability(const std::vector<std::vector<int>>& currentGrid, const std::vector<std::vector<int>>& previousGrid);
+double calculateEntropy(const std::vector<std::vector<int>>& grid);
