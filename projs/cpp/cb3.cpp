4c7b16bd16897dd5d892464cdf41db3daebdd415
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2024-07-30 15:53:54 +0100

    Add files via upload

---
+++ projs/cpp/cb3.cpp
@@ -0,0 +1,4 @@
+// Get the position and size of the paddle
+sf::Vector2f paddlePosition = paddle.getPosition(); // (x, y) coordinates of the top-left corner of the paddle
+float paddleWidth = paddle.getSize().x; // Width of the paddle
+float paddleHeight = paddle.getSize().y; // Height of the paddle
