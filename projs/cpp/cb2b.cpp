ab674dadd251cdc2618971881daeec257af7be9a
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2024-07-30 14:36:39 +0100

    Update cb2b.cpp

--- projs/cpp/cb2b.cpp
+++ projs/cpp/cb2b.cpp
@@ -1,3 +1,7 @@
+
+//Define the paddle width, fixed to 100 pixels for now
+const float paddleWidth = 100.0f;  
+
 // Create a paddle
 sf::RectangleShape paddle;
 initializePaddle(paddle, paddleWidth, windowHeight);
