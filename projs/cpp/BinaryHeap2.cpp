b24f097fbfb2102c9a2654dca9c80496dfe64ccb
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2024-08-25 21:15:17 +0100

    Create binh2.cpp

---
+++ projs/cpp/BinaryHeap2.cpp
@@ -0,0 +1,5 @@
+// heap: the heap vector, i: index to start heapifying down
+void heapifyDown(std::vector<int>& heap, int i);
+
+// heap: the heap vector, i: index to start heapifying up
+void heapifyUp(std::vector<int>& heap, int i);
