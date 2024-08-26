86c3addf0c19d275b151c5e584b99e448b7bfe42
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2024-08-26 10:03:49 +0100

    Create binh3.cpp

---
+++ projs/cpp/binh3.cpp
@@ -0,0 +1,8 @@
+// Inserts a new element into the heap
+void insert(std::vector<int>& heap, int value);
+
+// Removes the minimum element from the heap
+void removeMin(std::vector<int>& heap);
+
+// Returns the minimum element in the heap
+int getMin(const std::vector<int>& heap);
