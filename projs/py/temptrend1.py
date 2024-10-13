780b6cb7cca23782635db40f36ccb62bba329215
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2024-10-13 11:21:54 +0100

    Create temptrend1.py

---
+++ projs/py/temptrend1.py
@@ -0,0 +1,12 @@
+def longest_trend_length(data):
+    """
+    Computes the length of the longest increasing trend in the given data.
+    
+    Parameters:
+    data (list): A list of integers representing the sequence of temperature readings.
+    
+    Returns:
+    int: The length of the longest increasing trend.
+    """
+    # Placeholder for implementation
+    pass
