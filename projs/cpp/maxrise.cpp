b467fcade0c6e0827cde7d5025144694d26b01bf
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2024-10-26 10:53:26 +0100

    Create maxrise.cpp

---
+++ projs/cpp/maxrise.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+int maximum_rise(const std::vector<int>& prices) {
+    /**
+     * Computes the maximum rise in stock price between two time points.
+     *
+     * Args:
+     * prices (vector of int): List of stock prices at each time point.
+     *
+     * Returns:
+     * int: The maximum rise (prices[j] - prices[i]).
+     */
+
+    return -1;
+}
